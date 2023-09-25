d9cefb8f1abb9b726a05f6e7fcc883f2f50bb4e4
kenny <73383023+s1710931008@users.noreply.github.com>
2023-09-25 14:49:22 +0800

    0925

--- LongPolling/WebSocket1.js
+++ LongPolling/WebSocket1.js
@@ -1,7 +1,7 @@
 const WebSocket = require('ws');
 const http = require('http');
 
-// 创建HTTP服务器
+// 建立HTTP伺服器
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('WebSocket Server');
@@ -9,21 +9,22 @@ const server = http.createServer((req, res) => {
 
 const wss = new WebSocket.Server({ server });
 
-// 当有WebSocket连接建立时
+// 當有WebSocket連線建立時
 wss.on('connection', (ws) => {
-  console.log('客户端已连接');
+  console.log('客戶端已連接');
 
-  // 监听客户端的消息
+  // 監聽客戶端的訊息
   ws.on('message', (message) => {
-    console.log(`接收到消息: ${message}`);
+    console.log(`接收到訊息: ${message}`);
   });
 
-  // 模拟定时发送数据
+  // 模擬定時發送數據
   setInterval(() => {
-    ws.send(new Date().toString());
+    val=new Date().toString();
+    ws.send(val);
   }, 1000);
 });
 
 server.listen(3000, () => {
-  console.log('服务器正在运行在 http://localhost:3000');
+  console.log('伺服器正在運行在 http://localhost:3000');
 });
